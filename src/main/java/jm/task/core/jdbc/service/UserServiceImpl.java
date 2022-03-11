b4e54637590883fe7c5a7590e83080ea9f418c10
clctrulala <clctrulala@gmail.com>
2022-03-11 16:03:22 +0300

    dao with comments

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+// service переиспользует методы dao
+
+public class UserServiceImpl implements UserService {
+    private final UserDaoJDBCImpl userDAO = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDAO.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        userDAO.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        userDAO.saveUser(name, lastName, age);
+    }
+
+    public void removeUserById(long id) {
+        userDAO.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    public void cleanUsersTable() {
+        userDAO.cleanUsersTable();
+    }
+}
